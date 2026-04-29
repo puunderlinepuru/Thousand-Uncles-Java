7e2ec984eceabffd18b077da520e39b09004947c
puunderlinepuru <damnthatslitterallyme@gmail.com>
2026-04-29 20:57:20 +0300

    got simple dashboard website getting REST requests and forming queries to database. application.properties are safe because it's all local and I can already tell I WILL forget the data there :p

--- dashboard/src/main/java/com/thousand_uncles/dashboard/DashboardApplication.java
+++ dashboard/src/main/java/com/thousand_uncles/dashboard/DashboardApplication.java
@@ -9,6 +9,4 @@ public class DashboardApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DashboardApplication.class, args);
 	}
-
-
 }
---
+++ dashboard/src/main/java/com/thousand_uncles/dashboard/service/MapRecordService.java
@@ -0,0 +1,51 @@
+package com.thousand_uncles.dashboard.service;
+
+import com.thousand_uncles.dashboard.entity.MapRecord;
+import com.thousand_uncles.dashboard.repository.MapRecordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class MapRecordService {
+
+    @Autowired
+    @SuppressWarnings("unused")
+    private MapRecordRepository repository;
+
+//    // Add new record
+//    public MapRecord addRecord(String mapName) {
+//        MapRecord record = new MapRecord(mapName);
+//        return repository.save(record);
+//    }
+
+    // Read record by name
+    public MapRecord getRecordByName(String mapName) {
+        return repository.findByMapName(mapName);
+    }
+
+    // Read all records
+    public List<MapRecord> getAllRecords() {
+        return repository.findAll();
+    }
+
+    // Update record by name
+    public boolean updateRecord(String oldName, String newName) {
+        int updated = repository.updateMapName(oldName, newName);
+        return updated > 0;
+    }
+
+    // Delete record by name
+    public boolean deleteRecord(String mapName) {
+        int deleted = repository.deleteByMapName(mapName);
+        return deleted > 0;
+    }
+
+    // Search records by partial name
+    public List<MapRecord> searchRecords(String partialName) {
+        return repository.findByMapNameContaining(partialName);
+    }
+}
