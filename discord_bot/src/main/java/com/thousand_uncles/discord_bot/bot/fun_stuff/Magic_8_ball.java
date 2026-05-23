a82d0a160b8217256f7bedfea84197821d36d7fc
puunderlinepuru <damnthatslitterallyme@gmail.com>
2026-05-23 14:42:41 +0300

    there's now equal amount of yes/no answers. also total moved from 21 to 27

--- discord_bot/src/main/java/com/thousand_uncles/discord_bot/bot/fun_stuff/Magic_8_ball.java
+++ discord_bot/src/main/java/com/thousand_uncles/discord_bot/bot/fun_stuff/Magic_8_ball.java
@@ -6,17 +6,40 @@ import java.util.List;
 
 public class Magic_8_ball {
     static List<String> magic8Ball = List.of(
-            // Positive
-            "It is certain.", "It is decidedly so.", "Without a doubt.", " Yes – definitely.", "You may rely on it.",
-            "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", " Signs point to yes.",
+//          Positive [11]
+            "It is certain.",
+            "https://tenor.com/view/pyro-tf2-pyro-team-fortress-2-tf2-yippee-gif-11048667284588376403",
+            "Without a doubt.",
+            "Yes – definitely.",
+            "Most likely.",
+            "Probably yeah..",
+            "Yes.",
+            "Yes.. I think?",
+            "||Do it||",
+            "....Sure.",
+            "I don't got a PhD in whatever question you're asking me. So yes",
 
-            // Neutral
-            "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", " Cannot predict now.", "Concentrate and ask again.",
+//          Vagueposting [5]
+            "Are you stupid?",
+            "WAKE UP WAKE UP WAKE UP WAKE UP WAKE UP WAKE UP WAKE UP WAKE UP WAKE UP WAKE UP WAKE UP WAKE UP WAKE UP WAKE UP WAKE UP WAKE UP WAKE UP WAKE UP WAKE UP WAKE UP WAKE UP WAKE UP WAKE UP WAKE UP",
+            "Ummmm 42....Yeah. 42",
+            "ARE YOU THREATENING ME??!?",
+            "https://klipy.com/gifs/invincible-omniman-4",
 
-            // Negative
-            "Don’t count on it.", "My reply is no.", "My sources say no.", " Outlook not so good.", "Very doubtful.", "No."
+//          Negative [11]
+            "Don’t count on it.",
+            "Are you asking me for permission to do something stupid. The answer is No",
+            "My reply is no.",
+            "My sources say no.",
+            "Nuh uh",
+            "https://tenor.com/view/nope-videogame-construction-gif-4960956",
+            "https://tenor.com/view/sus-scout-lachen-tf2-gif-17981608274864336621",
+            "||Nope.||",
+            "Just don't",
+            "Very doubtful.",
+            "No."
     );
-    static int magic8BallSize = 21;
+    static final int magic8BallSize = 27;
 
     public static String getAnswers() {
         int n = GlobalThings.rand.nextInt(magic8BallSize);
