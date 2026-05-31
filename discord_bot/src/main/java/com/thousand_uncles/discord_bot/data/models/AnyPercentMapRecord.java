7c2c873ed87802256367f85adf92ffc93456bb52
puunderlinepuru <damnthatslitterallyme@gmail.com>
2026-05-31 22:06:53 +0300

    discord data changes

---
+++ discord_bot/src/main/java/com/thousand_uncles/discord_bot/data/models/AnyPercentMapRecord.java
@@ -0,0 +1,8 @@
+package com.thousand_uncles.discord_bot.data.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "any_percent")
+public class AnyPercentMapRecord extends MapRecord {}
