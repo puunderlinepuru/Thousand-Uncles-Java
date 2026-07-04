c954a763e55604a589210d0080bfea2e4b82fe68
puunderlinepuru <damnthatslitterallyme@gmail.com>
2026-07-04 13:46:34 +0300

    added pair and triple classes

---
+++ discord_bot/src/main/java/com/thousand_uncles/discord_bot/bot/util/Triple.java
@@ -0,0 +1,68 @@
+package com.thousand_uncles.discord_bot.bot.util;
+
+public class Triple<A, B, C> {
+
+    /**
+     * The first element of this <code>Pair</code>
+     */
+    private A first;
+
+    /**
+     * The second element of this <code>Pair</code>
+     */
+    private B second;
+
+    /**
+     * The third element of this <code>Pair</code>
+     */
+    private C third;
+
+    /**
+     * Constructs a new <code>Pair</code> with the given values.
+     *
+     * @param first  the first element
+     * @param second the second element
+     */
+    @SuppressWarnings("unused")
+    public Triple(A first, B second, C third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+
+    @SuppressWarnings("unused")
+    public Triple(){
+
+    }
+
+    public void setFirst(A first) {
+        this.first = first;
+    }
+
+    public A getFirst() {
+        return first;
+    }
+
+    public void setSecond(B second) {
+        this.second = second;
+    }
+
+    public B getSecond() {
+        return second;
+    }
+
+    public void setThird(C third) {
+        this.third = third;
+    }
+
+    public C getThird() {
+        return third;
+    }
+
+    @Override
+    public String toString() {
+        return first + ", " + second + ", " + third;
+    }
+
+    //getter for first and second
+}
